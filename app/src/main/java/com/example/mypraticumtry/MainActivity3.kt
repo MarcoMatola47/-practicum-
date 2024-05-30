240177122d23c5fb906d79187100198394357db7
MarcoMatola47 <marcomatola47@gmail.com>
2024-05-30 16:38:40 +0200

    first commit

--- app/src/main/java/com/example/mypraticumtry/MainActivity3.kt
+++ app/src/main/java/com/example/mypraticumtry/MainActivity3.kt
@@ -11,53 +11,54 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity3 : AppCompatActivity() {
 
+
     private lateinit var displaytextView: TextView
     private lateinit var averagetextView: TextView
-    private lateinit var Returnbutton:Button
-    private lateinit var Displaybutton:Button
-    private lateinit var Calculatebutton:Button
+    private lateinit var averagebutton:Button
+    private lateinit var backbutton:Button
+    private lateinit var displaybutton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
 
-        displaytextView = findViewById(R.id.displaytextView)
-        averagetextView = findViewById(R.id.averagetextView)
-        Returnbutton = findViewById(R.id.Returbutton)
-        Displaybutton = findViewById(R.id.Displaybutton)
-        Calculatebutton = findViewById(R.id.Calculatebutton)
 
 
 
-        Displaybutton.setOnClickListener {
+
+        displaytextView= findViewById(R.id.displaytextView)
+        averagetextView= findViewById(R.id.averagetextView)
+        displaybutton= findViewById(R.id.displaybutton)
+        averagebutton= findViewById(R.id.averagebutton)
+        backbutton= findViewById(R.id.backbutton)
+
+
+        displaybutton.setOnClickListener {
             val dateArray = intent.getStringArrayExtra("dateArray")
             val timeArrayMorning = intent.getStringArrayExtra("timeArrayMorning")
             val timeArrayAfternoon = intent.getStringArrayExtra("timesArrayAfternoon")
             val notesArray = intent.getStringArrayExtra("noteArray")
 
-            val details = StringBuilder()
-            var totalScreenTime = 0.0
+            val information = StringBuilder()
+            var totalscreentime=0.0
             for (int in dateArray?.indices ?: 0..0) {
                 val morningTime = timeArrayMorning?.get(int)?.toDoubleOrNull() ?: 0.0
                 val afternoonTime = timeArrayAfternoon?.get(int)?.toDoubleOrNull() ?: 0.0
-                totalScreenTime += morningTime + afternoonTime
+                totalscreentime += morningTime + afternoonTime
 
-                details.append("Date: ${dateArray?.get(int)}\n")
-                details.append("Morning Screen Time: $morningTime\n")
-                details.append("Afternoon Screen Time: $afternoonTime\n")
-                details.append("Notes: ${notesArray?.get(int)}\n\n")
+                information.append("Date: ${dateArray?.get(int)}\n")
+                information.append("Morning Screen Time: $morningTime\n")
+                information.append("Afternoon Screen Time: $afternoonTime\n")
+                information.append("Notes: ${notesArray?.get(int)}\n\n")
             }
+            displaytextView.text = information.toString()
 
-
-            displaytextView.text = details.toString()
-
-            val averageScreenTime = totalScreenTime / (dateArray?.size ?: 1)
-            averagetextView.text = "Average Screen Time: %.2f".format(averageScreenTime)
-
-
+            val averagescreentime = totalscreentime / (dateArray?.size ?: 1)
+            averagetextView.text = "Average Screen Time: %.2f".format(averagescreentime)
         }
-        Returnbutton.setOnClickListener {
+
+        backbutton.setOnClickListener {
             val intent= Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
